30e0dc97cc639a175009319cad482db541f9eb06
shlka <sazakurosulp36@gmail.com>
2025-09-27 11:55:25 +0900

    feat: implement main function and GA

--- src/main.py
+++ src/main.py
@@ -1,8 +1,101 @@
+import logging
+
 import numpy as np
+from opfunu.cec_based import cec2015
+
+logging.basicConfig(level=logging.INFO)
 
 
 def main():
-    pass
+    # parameters
+    dim = 10
+    seed = 1
+    knn = 5
+    rsm = 0.9
+
+    # benchmark function
+    f1 = cec2015.F12015(ndim=10)
+
+    ibrbf_ga(f1, dim, seed, knn, rsm)
+
+
+def ibrbf_ga(func, dim, seed, knn, rsm):
+    # search range
+    lb = -100
+    ub = 100
+
+    # random seed
+    np.random.seed(seed)
+
+    # parameters
+    maxfe = 200 * dim
+    popsize = 40
+    init_archive_size = 5 * dim
+    crossover_rate = 0.7
+    gamma = 0.4
+    mutation_rate = 0.3
+    num_cross = 2 * round(crossover_rate * popsize / 2)
+    num_mut = round(mutation_rate * popsize)
+
+    # initialize archive
+    archive_x = np.random.uniform(lb, ub, (init_archive_size, dim))
+    archive_y = np.array([func.evaluate(ind) for ind in archive_x])
+    archive_x = archive_x[np.argsort(archive_y)[::-1]]
+    archive_y = np.sort(archive_y)
+
+    # initialize population
+    pop = archive_x[:popsize]
+    fit = archive_y[:popsize]
+
+    fe = init_archive_size
+    logging.info(f"fbest: {fit[0]}, fe: {fe}")
+
+    while fe < maxfe:
+        randpop_idx = np.random.permutation(popsize)
+        parent = pop[randpop_idx]
+        parent_fit = fit[randpop_idx]
+
+        # crossover
+        parent_crossover = parent[randpop_idx[:num_cross]]
+        offscpring_crossover = []
+        for i in range(0, num_cross, 2):
+            p1 = parent_crossover[i]
+            p2 = parent_crossover[i + 1]
+
+            # TODO: implement BLX-alpha
+            offscpring_crossover.append(p1)
+            offscpring_crossover.append(p2)
+
+        # mutation
+        parent_mutation = parent[randpop_idx[num_cross:]]
+        offscpring_mutation = []
+        for i in range(num_mut):
+            p = parent_mutation[i]
+
+            # TODO: implement uniform mutation
+            offscpring_mutation.append(p)
+
+        offscpring = np.vstack((offscpring_crossover, offscpring_mutation))
+
+        psm = int(rsm * popsize)
+
+        # surrogate model
+        for i in range(popsize):
+            # TODO: implement surrogate model
+            # get training data for offspring[i]
+            # train RBF model
+            # predict offspring[i]
+            pass
+
+        # TODO: implement selection
+        # sort by predicted fitness
+        # select psm individuals to evaluate with the true function
+        fe = fe + psm
+        # add evaluated individuals to the archive
+        # select a best solution in parent
+        # update population and fitness
+
+        logging.info(f"fbest: {fit[0]}, fe: {fe}")
 
 
 if __name__ == "__main__":
