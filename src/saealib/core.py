f414127d0c19cd5eab808b9600045dc1633113bf
shlka <sazakurosulp36@gmail.com>
2025-10-03 14:57:38 +0900

    feat: add Population, Individual, Solution class

--- src/saealib/core.py
+++ src/saealib/core.py
@@ -2,12 +2,68 @@ import numpy as np
 
 
 class Individual:
-    pass
+    """
+    Individual class to handle single individual in population.
+    """
+    def __init__(self, population: "Population", index: int):
+        self.population = population
+        self.index = index
+
+    def get(self, key: str) -> np.ndarray:
+        if key not in self.population.data:
+            return None
+        return self.population.get(key)[self.index]
+
+    def set(self, key: str, value: np.ndarray) -> None:
+        self.population.get(key)[self.index] = value
+
+
+class Solution(Individual):
+    """
+    Solution class to generate single individual easily.
+    """
+    def __init__(self, x: np.ndarray, **kwargs):
+        pop = Population.new("x", np.array([x]))
+        for k, v in kwargs.items():
+            pop.set(k, np.array([v]))
+        super().__init__(pop, 0)
+
 
 
 class Population:
-    pass
+    """
+    Base class for population.
+    (self.data must have at least "x" key.)
+    """
+    def __init__(self):
+        self.data = {}
+    
+    @staticmethod
+    def new(key: str, value: np.ndarray) -> "Population":
+        pop = Population()
+        pop.set(key, value)
+        return pop
 
+    def get(self, key: str) -> np.ndarray:
+        if key not in self.data:
+            return None
+        return self.data.get(key)
+
+    def set(self, key: str, value: np.ndarray) -> None:
+        self.data[key] = value
+
+    def __len__(self):
+        if "x" in self.data:
+            return self.data["x"].shape[0]
+        return 0
+    
+    def __getitem__(self, index):    
+        if isinstance(index, int):
+            return Individual(self, index)
+        elif isinstance(index, slice):
+            return [Individual(self, i) for i in range(*index.indices(len(self)))]
+        else:
+            raise TypeError("Invalid argument type.")
 
 class Archive:
     pass
