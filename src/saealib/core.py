e024daf29d69c14d8d3de86ddc7e37dab739a648
shlka <sazakurosulp36@gmail.com>
2025-10-09 08:30:46 +0900

    fix: (RBFsurrogate) change logging message

--- src/saealib/core.py
+++ src/saealib/core.py
@@ -304,11 +304,11 @@ class RBFsurrogate(Surrogate):
                 self.kernel_matrix[i, j] = self.kernel(train_x[i], train_x[j])
         # self.weights = np.linalg.solve(self.kernel_matrix, train_y)
         if np.linalg.cond(self.kernel_matrix) > 1 / np.finfo(self.kernel_matrix.dtype).eps:
-            logging.warning("Kernel matrix is ill-conditioned. Adding regularization.")
+            logging.warning("Kernel matrix is ill-conditioned.")
         try:
             self.weights = np.linalg.solve(self.kernel_matrix, (train_y - np.mean(train_y)))
         except np.linalg.LinAlgError:
-            logging.error("Failed to solve linear system.")
+            logging.error("Failed to solve linear system (Kernel matrix might be singular).")
             self.weights = np.zeros(n_samples)
 
     def predict(self, test_x):
