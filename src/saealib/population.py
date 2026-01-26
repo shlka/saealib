a7d88e5d7ea6f7b1c6c3c436ba5239ac66d4d878
shlka <sazakurosulp36@gmail.com>
2026-01-26 17:03:58 +0900

    feat: Changing the design to the SoA pattern, add method "append", "extend", "extract", "truncate", "delete", "reorder", "argsort", "clear", "empty_like" and some property, change method "get", delete method "new", "merge", "set"

--- src/saealib/population.py
+++ src/saealib/population.py
@@ -14,6 +14,9 @@ import warnings
 import numpy as np
 
 
+T_Population = TypeVar("T_Population", bound="Population")
+
+
 @dataclass(frozen=True)
 class PopulationAttribute:
     """
@@ -38,90 +41,356 @@ class PopulationAttribute:
 
 class Population:
     """
-    Base class for population.
-    (self.data must have at least "x" key.)
+    Container for population data.
 
     Attributes
     ----------
-    data : dict[str, np.ndarray]
-        Dictionary to store population data.
+    schema : Dict[str, PopulationAttribute]
+        Schema defining the attributes of the population.
+    _data : Dict[str, np.ndarray]
+        Dictionary to store population data arrays.
+    _capacity : int
+        Current capacity of the population.
+    _size : int
+        Current size of the population.
+    _version : int
+        Version number to track modifications.
     """
-    def __init__(self):
-        self.data = {}
-    
-    @staticmethod
-    def new(key: str, value: np.ndarray) -> Population:
-        pop = Population()
-        pop.set(key, value)
-        return pop
-
-    @classmethod
-    def merge(cls, *populations: Population) -> Population:
+    def __init__(self, attrs: List[PopulationAttribute], init_capacity: int = 100) -> None:
         """
-        Merge multiple populations into new Population.
-
-        Only keys common to all populations are merged.
+        Initialize a Population.
 
         Parameters
         ----------
-        *populations : Population
-            Populations to merge.
-
-        Returns
-        -------
-        Population
-            Merged Population.
+        attrs : List[PopulationAttribute]
+            List of population attributes. Each attribute defines a column in the population.
+        init_capacity : int, optional
+            Initial capacity of the population, by default 100.
         """
-        if not populations:
-            return cls()
-        
-        all_keys = set().union(*(pop.data.keys() for pop in populations))
+        # checking duplicate attrs
+        reserved_names = set(dir(self.__class__)) | set(self.__dict__.keys())
+        for attr in attrs:
+            if attr.name in reserved_names:
+                warnings.warn(
+                    f"Attribute name '{attr.name}' conflicts with a Population method/property. "
+                    f"Access via pop.{attr.name} will return the method. "
+                    f"Use pop.get('{attr.name}') or pop.get_array('{attr.name}') to access the data.",
+                    UserWarning,
+                    stacklevel=3
+                )
+        self._capacity = init_capacity
+        self._size = 0
+        self._version = 0
+        self._data: Dict[str, np.ndarray] = {}
+        for attr in attrs:
+            self._init_column(attr, self._capacity)
+        self._schema = {attr.name: attr for attr in attrs}
 
-        new_pop = cls()
-        for key in all_keys:
-            merged_arr = [p.get(key) for p in populations if p.get(key) is not None]
-            if len(merged_arr) == len(populations):
-                new_pop.set(key, np.vstack(merged_arr))
+    def _init_column(self, attr: PopulationAttribute, capacity: int) -> None:
+        """
+        Initialize a column in the population.
+
+        Parameters
+        ----------
+        attr : PopulationAttribute
+            The attribute definition for the column.
+        capacity : int
+            The initial capacity of the column.
+        """
+        shape = (capacity, ) + attr.shape
+        if attr.default is not None:
+            arr = np.full(shape=shape, fill_value=attr.default, dtype=attr.dtype, order="C")
+        elif np.issubdtype(attr.dtype, np.floating) and np.isnan(attr.default):
+            arr = np.full(shape=shape, fill_value=np.nan, dtype=attr.dtype, order="C")
+        else:
+            arr = np.zeros(shape=shape, dtype=attr.dtype, order="C")
+        self._data[attr.name] = arr
+
+    def _resize(self, new_capacity: int) -> None:
+        """
+        Resize the population to a new capacity.
+
+        Parameters
+        ----------
+        new_capacity : int
+            The new capacity of the population.
+        """
+        for k, v in self._data.items():
+            attr = self._schema[k]
+            shape = (new_capacity, ) + attr.shape
+            new_arr = np.full(shape=shape, fill_value=attr.default, dtype=attr.dtype, order="C")
+            if attr.default is not None:
+                new_arr[:] = attr.default
+            new_arr[:self._size] = v[:self._size]
+            self._data[k] = new_arr
+        self._capacity = new_capacity
+
+    def append(self, element: Individual | Dict[str, Any] | None = None, **kwargs) -> None:
+        """
+        Append a new individual to the population.
+
+        Parameters
+        ----------
+        element : Individual | dict | None
+            Data for the additional individual
+        **kwargs : 
+            Set attribute values individually and add them. Alternatively, overwrite based on the element's value and add it.
         
+        Example
+        -------
+        >>> pop.append(ind)             # adding ind(Individual)
+        >>> pop.append({"x": x_val})    # adding dict
+        >>> pop.append(x=x_val, f=0.1)  # adding with keywords
+        >>> pop.append(ind, f=0.1)      # Overwrite only the 'f' based on the ind and add it.
+        """
+        # data: merge element and kwargs
+        data: Dict[str, Any] = {}
+        if element is not None:
+            if isinstance(element, dict):
+                data.update(element)
+            else:
+                for key in self._schema:
+                    if hasattr(element, key):
+                        data[key] = getattr(element, key)
+        data.update(kwargs)
+
+        # resizing
+        if self._size >= self._capacity:
+            self._resize(self._capacity * 2)
+
+        # appending
+        idx = self._size
+        for key, attr in self._schema.items():
+            data_self = self._data[key]
+            if key in data:
+                data_self[idx] = data[key]
+            else:
+                # fill default values
+                if attr.default is not None:
+                    data_self[idx] = attr.default
+                elif np.issubdtype(attr.dtype, np.floating) and np.isnan(attr.default):
+                    data_self[idx] = np.nan
+                else:
+                    data_self[idx] = 0
+
+        self._size += 1
+        self._version += 1
+
+    def extend(self, other: Self | dict) -> None:
+        """
+        Extend this population with another population.
+
+        Parameters
+        ----------
+        other : Population | dict
+            The other population to extend from.
+        """
+        # Population(Self) or dict
+        if isinstance(other, (Self, Population)):
+            other_size = len(other)
+            other_data = {k: other.get_array(k) for k in other.schema}
+        elif isinstance(other, dict):
+            other_size = len(next(iter(other.values())))
+            other_data = other
+
+        # if empty extended
+        if other_size == 0: return
+
+        # resizing
+        if self._size + other_size > self._capacity:
+            self._resize(max(self._capacity * 2, self._size + other_size))
+
+        # extending
+        start = self._size
+        for key, attr in self._schema.items():
+            val_self = self._data[key]
+            if key in other_data:
+                val_self[start:start+other_size] = other_data[key]
+            else:
+                # fill default values
+                if attr.default is not None:
+                    val_self[start:start+other_size] = attr.default
+                elif np.issubdtype(attr.dtype, np.floating) and np.isnan(attr.default):
+                    val_self[start:start+other_size] = np.nan
+                else:
+                    val_self[start:start+other_size] = 0
+        
+        self._size += other_size
+        self._version += 1
+
+    def extract(self, indices: np.ndarray | List[int] | slice) -> Self:
+        """
+        Extract individuals with indices, and return new Population.
+
+        Parameters
+        ----------
+        indices : np.ndarray | List[int] | slice
+            Indices to extract
+        """
+        if isinstance(indices, slice):
+            start, stop, step = indices.indices(self._size)
+            n_extract = len(range(start, stop, step))
+            indices_arr = slice(start, stop, step)
+        else:
+            indices_arr = np.array(indices)
+            n_extract = len(indices_arr)
+        
+        new_pop = self.empty_like(capacity=n_extract)
+
+        for key, val in self._data.items():
+            new_pop._data[key][:n_extract] = val[:self._size][indices_arr]
+        
+        new_pop._size = n_extract
+        new_pop._version += 1
         return new_pop
 
-
-    def get(self, key: str) -> np.ndarray:
+    def truncate(self, new_size: int) -> None:
         """
-        Get the population data for the given key.
+        Cut the population to a new size.
+
+        Parameters
+        ----------
+        new_size : int
+            The new size of the population.
+        """
+        if new_size < 0:
+            raise ValueError("new_size must be non-negative")
+        if new_size < self._size:
+            self._size = new_size
+            self._version += 1
+
+    def delete(self, index: int | slice | List[int] | np.ndarray) -> None:
+        """
+        Delete individuals from the population.
+
+        Parameters
+        ----------
+        index : int, slice, list[int], np.ndarray
+            The index or indices of individuals to delete.
+        """
+        bool_mask = np.ones(self._size, dtype=bool)
+        bool_mask[index] = False
+        new_size = np.sum(bool_mask)
+        for k, v in self._data.items():
+            valid_data = v[:self._size]
+            v[:new_size] = valid_data[bool_mask]
+        self._size = new_size
+        self._version += 1
+
+    def reorder(self, order: np.ndarray) -> None:
+        """
+        Reorder individuals in the population.
+
+        Parameters
+        ----------
+        order : np.ndarray
+            The new order of individuals.
+        """
+        if len(order) != self._size:
+            raise ValueError(f"Order length {len(order)} must match population size {self._size}")
+        for k, v in self._data.items():
+            valid_data = v[:self._size]
+            v[:self._size] = valid_data[order]
+        self._version += 1
+
+    def argsort(self, name: str, reverse: bool = False) -> np.ndarray:
+        """
+        Get the indices that would sort the population by a specific attribute.
+
+        Parameters
+        ----------
+        name : str
+            The attribute name to sort by.
+        reverse : bool, optional
+            Whether to sort in descending order, by default False.
+        """
+        if name not in self._data:
+            raise KeyError(f"Key '{name}' not found in population schema")
+        sort_arg = np.argsort(self._data[name][:self._size])
+        if reverse:
+            sort_arg = sort_arg[::-1]
+        return sort_arg
+
+    def clear(self) -> None:
+        """
+        Clear the population.
+        """
+        self._size = 0
+        self._version += 1
+
+    def empty_like(self, capacity: int = None):
+        """
+        Create emplty Population object that have same schema
+
+        Parameters
+        ----------
+        capacity : int
+            capacity of new Population object. default is self._capacity.
+        """
+        if capacity is None:
+            capacity = self._capacity 
+        return self.__class__(self.attrs, capacity)
+
+    def get(self, key: str, default = None) -> np.ndarray:
+        """
+        Get the array of a specific attribute.
+        If the key does not exist in the attribute, it returns the specified value (default is None).
 
         Parameters
         ----------
         key : str
-            The key to retrieve data for.
+            The attribute name to get the array for.
+        default : Any
+            If the key does not exist in the attribute, return this value.
         """
-        if key not in self.data:
-            return None
-        return self.data.get(key)
+        if key in self._data:
+            return self.get_array(key)
+        return default
 
-    def set(self, key: str, value: np.ndarray) -> None:
+    def get_array(self, key: str) -> np.ndarray:
         """
-        Set the population data for the given key.
+        Get the array of a specific attribute.
 
         Parameters
         ----------
         key : str
-            The key to set data for.
-        value : np.ndarray
-            The data to set for the given key.
+            The attribute name to get the array for.
         """
-        self.data[key] = value
+        return self._data[key][:self._size]
 
-    def __len__(self):
-        if not self.data:
-            return 0
-        return len(next(iter(self.data.values())))
+    @property
+    def schema(self) -> MappingProxyType[str, PopulationAttribute]:
+        return MappingProxyType(self._schema)
 
-    def __getitem__(self, index):    
+    @property
+    def attrs(self) -> List[PopulationAttribute]:
+        return list(self._schema.values())
+
+    def __len__(self) -> int:
+        return self._size
+
+    def __getattr__(self, name: str) -> np.ndarray:
+        """support dot access (ex: pop.x)"""
+        if name in self._data:
+            return self.get_array(name)
+
+        raise AttributeError(f"'{type(self).__name__}' object has no attribute '{name}'")
+
+    def __getitem__(self, index: int | slice) -> Individual | Population:
+        """
+        support bracket access
+
+        pop[0]   -> return Individual
+        pop[:10] -> return new Population
+        """
+        # index (return Individual)
         if isinstance(index, int):
+            if index < 0 or index >= self._size:
+                raise IndexError("Index out of range")
             return Individual(self, index)
+        # slice (return Population)
         elif isinstance(index, slice):
-            return [Individual(self, i) for i in range(*index.indices(len(self)))]
+            return self.extract(index)
         else:
             raise TypeError("Invalid argument type.")
 
