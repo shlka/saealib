143d30e5d08023d476f618063200ceb68830ad03
shlka <shlka@users.noreply.github.com>
2026-08-24 17:15:29 +0000

    deploy: b021da0ff9be2230741c3f1a1ddcfe2a3de461d3

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.0b5.dev85+gadbfcdf9f',
+    VERSION: '0.1.0b5.dev86+gb021da0ff',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
